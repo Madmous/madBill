1fb4834d706b0554231012c6d1065b7ac9d4bef7
madmous <mail@madmous.com>
2019-04-14 18:04:19 +0200

    feat(server): create : route

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,4 +2,11 @@ import express from "express";
 
 const router = express();
 
+router.get("/", (_, res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  res.status(200);
+  res.send({ message: "hello world" });
+});
+
 export default router;
