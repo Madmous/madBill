12440f2d4f61925755f7ebd5f3326afdafeea606
madmous <mail@madmous.com>
2019-04-19 13:44:48 +0200

    feat(web): rename form folder to Form

--- web/src/price/total.ts
+++ web/src/price/total.ts
@@ -1,4 +1,4 @@
-import { Item } from '../app/oldform';
+import { Item } from '../app/Form';
 
 export const calculateTotal = (items: Item[]): number =>
   items.reduce((acc, item) => acc + Number(item.quantity) * Number(item.unitPrice) * Number(item.amount) * 1.2, 0);
