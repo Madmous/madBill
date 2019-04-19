b536e11d870648cca6bd6b45b60a3a2097e6240c
madmous <mail@madmous.com>
2019-04-19 17:40:55 +0200

    feat(server): create save invoice endpoint

---
+++ server/src/__tests__/index.spec.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+
+import startServer from '../index';
+
+const aPayload = (props: any) => {
+  const defaultPayload = {
+    billTo: '2 Avenue Madmous',
+    dueDate: '02/02/2019',
+    from: 'Dakar',
+    invoiceDate: '02/02/2019',
+    invoiceNumber: 'AS33',
+    items: [
+      {
+        amount: 1,
+        description: 'mango',
+        quantity: 1,
+        unitPrice: 1,
+      },
+    ],
+    shipTo: 'Paris',
+  };
+
+  return {
+    ...defaultPayload,
+    ...props,
+  };
+};
+
+describe('POST /save-invoice', () => {
+  [{}, aPayload({ from: 2 })].forEach(payload => {
+    it('should not work when payload is either missing or not correct', done => {
+      request(startServer)
+        .post('/save-invoice')
+        .send(payload)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then(response => {
+          expect(response.body.message).toContain('fails because');
+          done();
+        });
+    });
+  });
+
+  it('should work when payload is correct', done => {
+    request(startServer)
+      .post('/save-invoice')
+      .send(aPayload({}))
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(_ => {
+        done();
+      });
+  });
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,46 @@
-import express from "express";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import Joi from 'joi';
 
 const router = express();
 
-router.get("/", (_, res) => {
-  res.setHeader("Content-Type", "application/json");
+router.use(cors({ origin: true, credentials: true }));
 
-  res.status(200);
-  res.send({ message: "hello world" });
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+router.post('/save-invoice', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  const itemSchema = Joi.object().keys({
+    amount: Joi.number().required(),
+    description: Joi.string().required(),
+    quantity: Joi.number().required(),
+    unitPrice: Joi.number().required(),
+  });
+
+  const schema = Joi.object().keys({
+    billTo: Joi.string().required(),
+    dueDate: Joi.string().required(),
+    from: Joi.string().required(),
+    invoiceDate: Joi.string().required(),
+    invoiceNumber: Joi.string().required(),
+    items: Joi.array()
+      .items(itemSchema)
+      .required(),
+    shipTo: Joi.string().required(),
+  });
+
+  Joi.validate(req.body, schema, (err, _) => {
+    if (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    } else {
+      res.status(200).send();
+    }
+  });
 });
 
 export default router;
